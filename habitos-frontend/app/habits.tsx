eb730b7a2dfe9fcfa03823a5be1aa4110c76477c
Gianluca Hernandez <24002037@galileo.edu>
2025-03-25 00:55:36 -0600

    agregando cambios frontend

--- habitos-frontend/app/habits.tsx
+++ habitos-frontend/app/habits.tsx
@@ -1,15 +1,36 @@
+import { useSelector, useDispatch } from "react-redux";
+import { markAsDoneThunk } from "@/features/habit/habitSlice";
+import { AppDispatch, RootState } from "@/Redux/store";
+import { fetchHabitsThunk } from "@/features/habit/habitSlice";
+
 type Habit = {
     _id: string;
     title: string;
     description: string;
+    createdAt: string;
+    days: number;
+    lastDone: Date;
+    lastUpdated: Date;
 }
 
-type HabitState = {
+type HabitsProps = {
     habits: Habit[];
 }
 
-export default function Habits({habits}: HabitState) {
+const handleMarkAsDone = (habitId: string, dispatch: AppDispatch) => {
+  dispatch(markAsDoneThunk(habitId));
+  dispatch(fetchHabitsThunk());
+}
 
+export default function Habits({habits}: HabitsProps) {
+  const dispatch = useDispatch<AppDispatch>();
+  const status = useSelector((state: RootState) => state.habits.status);
+  const error = useSelector((state: RootState) => state.habits.error);
+
+  const calcularProgress = (days: number): number => {
+    return Math.min((days / 66) * 100, 100);
+  }
+  
   return (
     <div className="w-full max-w-md p-4 bg-white rounded-lg shadow-md mt-8">
       <h1 className="text-2xl font-bold mb-4 text-black">Habits</h1>
@@ -18,8 +39,10 @@ export default function Habits({habits}: HabitState) {
             <li className="flex items-center justify-between" key={habit._id}>
                 <span className="text-black">{habit.title}</span>
                 <div className="flex items-center space-x-2">
-                  <progress className="w-50" value="70" max="100"> </progress>
-                  <button className="relative px-2 py-1 text-lg font-bold text-white uppercase transition-all duration-300 bg-blue-600 border border-blue-400 rounded-lg shadow-lg hover:bg-blue-700 hover:shadow-blue-500/50 active:scale-95">Done</button>
+                  <progress className="w-30" value={calcularProgress(habit.days)} max="100"> </progress>
+                  <button className="relative px-3 py-2 text-sm font-bold text-white uppercase transition-all duration-300 bg-blue-400 border border-blue-200 rounded-lg shadow-lg hover:bg-blue-700 hover:shadow-blue-500/50 active:scale-95"onClick={() => handleMarkAsDone(habit._id,dispatch)}>{status[habit._id] === "loading"?"Processing" : "Done"} </button>
+                  {status[habit._id] === "failed" && <span className="text-red-500">{error[habit._id]}</span>}
+                  {status[habit._id] === "success" && <span className="text-green-500">Already marked as done!</span>}
                 </div>
             </li>
           ))}
